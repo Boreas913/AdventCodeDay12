c08a6280de3954be478c8975632064e0d1f721d8
Aurora Smith <Auroracsmith913@gmail.com>
2022-12-12 23:33:02 -0700

    all for nothing

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string input = File.ReadAllText("Calories.txt");
+System.Console.WriteLine(PartOne());
+System.Console.WriteLine(PartTwo());
+
+long PartOne()
+{
+    long highestCalories = input
+        .Split("\r\n\r\n")
+        .Select(elf => elf
+            .Split("\r\n", StringSplitOptions.RemoveEmptyEntries)
+            .Aggregate(0L, (acc, value) => acc + long.Parse(value)))
+        .Max();
+
+    return highestCalories;
+}
+long PartTwo()
+{
+    long highestThreeElves = input
+        .Split("\r\n\r\n")
+        .Select(elf => elf
+            .Split("\r\n", StringSplitOptions.RemoveEmptyEntries)
+            .Aggregate(0L, (acc, value) => acc + long.Parse(value)))
+        .OrderDescending()
+        .Take(3)
+        .Sum();
+
+    return highestThreeElves;
+}
+
+
+
